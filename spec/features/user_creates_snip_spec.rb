b92729967087b69e86c0af2a7e2c6b35334027ba
Natt Eyre <terskaya.nv@gmail.com>
2015-07-15 00:48:40 +0300

    Moved human strings to i18n

--- spec/features/user_creates_snip_spec.rb
+++ spec/features/user_creates_snip_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 feature "User creates a snip" do
   scenario "successfully" do
     visit root_path(as: create(:user))
-    click_on "New Snip"
-    fill_in "Name", with: "S1"
-    fill_in "Content", with: "test"
-    click_on "Create a snip"
+    click_on t("snips.index.add")
+    fill_in Snip.human_attribute_name("name"), with: "S1"
+    fill_in Snip.human_attribute_name("content"), with: "test"
+    click_on t("snips.new.add")
 
     expect(page.current_path).to eq(snips_path)
     expect(page).to have_content("S1")
