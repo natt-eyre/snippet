52c600d7efadf98f9710ae05324641b3f7a85874
Natt Eyre <terskaya.nv@gmail.com>
2015-07-20 01:07:12 +0300

    Added spec for comments#create

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -12,5 +12,15 @@ describe CommentsController do
       expect(snip.comments.count).to eq(1)
       expect(snip.comments.last.comment). to eq("test")
     end
+
+    it "creates comments for current user" do
+      user = create(:user)
+      snip = create(:snip, user: user)
+
+      sign_in_as user
+      post :create, { snip_id: snip, comment: { title: "", comment: "test"} }
+
+      expect(Comment.last.user).to eq(user)
+    end
   end
 end
